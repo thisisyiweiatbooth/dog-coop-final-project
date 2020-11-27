05943274f8e3832b0c55dd8c8480f9b68208dd62
thisisyiwei <yi.wei@chicagobooth.edu>
2020-11-27 01:26:41 +0000

    tried to create conditional for deleting appt/ no method for dog_owner for some reason

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -15,17 +15,24 @@ class Appointment < ApplicationRecord
 
   belongs_to(:dog_watcher, { :required => false, :class_name => "HomoSapien", :foreign_key => "dog_watcher_id", :counter_cache => true })
 
-  # belongs_to(:dog, { :required => false, :class_name => "Dog", :foreign_key => "dog_id", :counter_cache => true })
+  belongs_to(:dog, { :required => false, :class_name => "Dog", :foreign_key => "dog_id", :counter_cache => true })
 
   validates(:start_date, { :presence => true })
 
   validates(:end_date, { :presence => true })
 
+  def dog_owner
+    dog_in_appt = Dog.where({ :id => self.dog_id}).at(0)
+    return HomoSapien.where({ :id => dog_in_appt.owner_id}).at(0)
+  end
+
   # validates(:dog_watcher_id, { :numericality => true })
 
-  def dog
-   return Dog.where({ :id => self.dog_id}).at(0)
-  end
+  # def dog
+  #  return Dog.where({ :id => self.dog_id}).at(0)
+  # end
+
+ 
 
   # def requester
   #   dog_requested = Dog.where({ :id => self.dog_id}).at(0)
